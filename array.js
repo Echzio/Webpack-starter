62fdb3b740988c1e5cb67bc5867e4136d34b81be
Echzio <aleksassincreed@yandex.ru>
2018-11-07 20:59:10 +0300

    require testing

---
+++ array.js
@@ -0,0 +1,5 @@
+var array_counter = function (array) {
+    return "В массиве находится " + array.length + " элементов!";
+};
+
+console.log(array_counter([1, 2, 3, 4, 5, 6, 7, 8, 9]));
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("Hello world!");
 console.log(__dirname);
 console.log(__filename);
+
+let = function test(a) {
+    console.log("Ты написал: " + a);
+}
+
+let('привет');
+
+require('./array');
