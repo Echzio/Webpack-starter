6b5dc33a576e0ad38919bd1cd7c1df95c5055eb9
Echzio <aleksassincreed@yandex.ru>
2018-11-12 22:31:59 +0300

    build app.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// basic vars
+// операция с путями
+const path = require('path');
+const webpack = require('webpack');
+
+// доп. плагины или модули
+
+// module settings
+module.exports = {
+    // базовый путь к проекту
+    context: path.resolve(__dirname, 'src'),
+
+    // настройка точек входа в js
+    entry: {
+        // основной файл приложения
+        app: [
+            './js/app.js'
+        ],
+    },
+
+    // путь для собранных файлов
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist/js/'),
+        publicPath: '../'
+    },
+}
