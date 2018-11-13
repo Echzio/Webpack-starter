8d33223459198a0e788193e62aa301234a0fd7df
Echzio <aleksassincreed@yandex.ru>
2018-11-13 11:13:11 +0300

    пересборка webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 // доп. плагины или модули
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 // module settings
 module.exports = {
@@ -15,8 +15,7 @@ module.exports = {
     entry: {
         // основной файл приложения
         app: [
-            './js/app.js',
-            './scss/style.scss'
+            './js/app.js'
         ],
     },
 
@@ -29,36 +28,8 @@ module.exports = {
 
     // dev server configuration
     devServer: {
-        contentBase: './app'
-    },
-
-    module: {
-        rules: [{
-            // если webpack находит scss документы, то
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-                use: [{
-                        loader: 'css-loader',
-                        option: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        option: {
-                            sourceMap: true
-                        }
-                    }
-                ],
-                fallback: 'style-loader',
-            })
-        }],
-
-    },
-
-    plugins: [
-        new ExtractTextPlugin(
-            './css/style.css'
-        ),
-    ],
+        contentBase: path.join(__dirname, 'app'),
+        port: 3333,
+        compress: true
+    },   
 }
