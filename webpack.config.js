76a88b8a7ae0494ced22480949be40d8de09d6e8
Alexander <Aleksassincreed@yandex.ru>
2019-01-18 13:05:12 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
-const webpack = require('webpack';)
+const webpack = require('webpack');
 
 module.exports = {
     // вход
