99475279f39d35b73a8a17b5919ba41baa0e3b10
Echzio <aleksassincreed@yandex.ru>
2019-02-01 21:07:03 +0300

    [ADD] Multiple EntryPoint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-//для путей
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -6,66 +5,77 @@ const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 
 module.exports = {
-    // вход
-    entry: ['@babel/polyfill', './src/index.js'],
-    // выход
+    entry: {
+        main: ['@babel/polyfill', './src/index.js'],
+        style: './src/scss/style.scss',
+        style2: './src/scss/style-two.scss',
+        style3: './src/scss/style-three.scss',
+    },
+
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'awesome.js',
-        // относительная ссылка на итоговый файл
+        filename: 'bundle.js',
         publicPath: '',
     },
     devServer: {
-        // для показа ошибки вне консоли
         overlay: true,
         hot: true,
-        port: 9000
+        port: 9000,
     },
     module: {
-        rules: [{
+        rules: [
+
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
                 }
             },
             {
-                test: /\.scss$/,
-                use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: MiniCssExtractPlugin.loader
-                }, {
-                    loader: 'css-loader',
-                    options: {
-                        sourceMap: true
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+
+                    {
+                        loader: 'style-loader',
+                    }, {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+
+                        }
+                    }, {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                        }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    browsers: ['ie >= 8', 'last 4 version'],
+                                })
+                            ],
+                            sourceMap: true,
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
                     }
-                }, {
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins: [
-                            autoprefixer({
-                                browsers: ['ie >= 8', 'last 4 version']
-                            })
-                        ],
-                        sourceMap: true
-                    }
-                }, {
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                }]
+                ]
             },
             {
                 test: /\.(png|jpg)$/,
-                loader: 'url-loader'
+                loader: 'url-loader',
             }
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'css/style.css',
+            filename: 'css/[name].css',
         }),
         new HtmlWebpackPlugin({
             template: './src/index.html'
